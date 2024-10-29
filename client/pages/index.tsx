20d519ec89114d4aaaf12aca830e4ed641322c14
Vivek Somarapu <81649577+vivek-somarapu@users.noreply.github.com>
2024-10-29 16:16:24 -0500

    test commit for remote repo deployment

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,6 +8,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Separator } from "@/components/ui/separator"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+// temp
 
 const menuItems = {
   combos: ["Bowl", "Plate", "Bigger Plate", "Biggest Plate"],
