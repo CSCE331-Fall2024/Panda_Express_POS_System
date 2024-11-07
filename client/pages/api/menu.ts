772e952beef0b3f9a42411190481b0d12b400190
Vivek Somarapu <81649577+vivek-somarapu@users.noreply.github.com>
2024-11-07 13:39:48 -0600

    dynamically render menu items from the database in correct order, update database for easy rendering

---
+++ client/pages/api/menu.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.PSQL_USER,
+  host: process.env.PSQL_HOST,
+  database: process.env.PSQL_DATABASE,
+  password: process.env.PSQL_PASSWORD,
+  port: Number(process.env.PSQL_PORT),
+  ssl: { rejectUnauthorized: false },
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+      try {
+        const result = await pool.query('SELECT * FROM menu_item ORDER BY menu_item_id');
+        const menuItems = result.rows.map(item => ({
+            ...item,
+            price: Number(item.price),
+          }));
+          res.status(200).json({ success: true, menuItems });
+      } catch (error) {
+        console.error('Error fetching menu items:', error);
+        res.status(500).json({ success: false, message: 'Failed to fetch menu items' });
+      }
+    } else {
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  }
