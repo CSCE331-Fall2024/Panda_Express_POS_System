f0ec9e121cf88c27af5f6291c26a03d2d7294677
Elizabeth Thomas <elizabethathomas03@gmail.com>
2024-11-19 23:20:45 -0600

    updated main landing page to start reworking menu boards. add placehold text to manager landing page

--- client/pages/manager/index.tsx
+++ client/pages/manager/index.tsx
@@ -12,6 +12,44 @@ const Manager: React.FC = () => {
     <ManagerNavBar />
     <div style={pageStyle}>
       <div style={overlayStyle}></div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '20px',
+          marginTop: '100px',
+          width: '100%',
+          maxWidth: '100%',
+          height: '30%',
+          maxHeight: '75%',
+          zIndex: 20,
+        }}
+      >
+        {/* Welcome Message-- placeholder text for now */}
+        <h1
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontSize: '50px',
+            fontWeight: 'bold',
+            color: 'white',
+            marginBottom: '10px',
+          }}
+        >
+          Welcome to the Manager View.
+        </h1>
+        <h2
+        style={{
+          fontSize:'20px',
+          paddingBottom: '20px',
+          fontStyle: 'italic',
+          color: 'white'
+        }}>
+          Navigate to the ☰ to begin.
+        </h2>
+        </div>
       </div>
       </>
   );
